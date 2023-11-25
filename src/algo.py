1175d3cf7f03effebefe98eafc39bcc83b28aeab
Yinon Horev <yinon.horev@Yinons-MacBook-Pro.local>
2023-11-25 16:29:35 +0100

    Added naive implemention, walking skeleton ready

--- src/algo.py
+++ src/algo.py
@@ -1,58 +1,54 @@
-from collections import defaultdict
 import logging
-from typing_extensions import DefaultDict
 
 import networkx as nx
 
-from api import PathAnalysisRequest
-
-
 logger = logging.getLogger(__name__)
+
 
 class PathAnalyser:
     def __call__(self, G: nx.DiGraph, e1: str, h: str) -> set[str]:
         self._validate_input(G, e1, h)
-        self.visited = defaultdict(bool)
-        self.depth = defaultdict(int)
-        self.low = defaultdict(int)
-        self.result = set()
-        self.h_found = False
-        self._articulation_points(G, e1, h, 0)
-        if not self.h_found:
-            msg = f"{h!r} is not reachable from {e1!r}"
-            logger.error(msg)
-            raise ValueError(msg)
-        return self.result
-
+        return self._naive_solution(G, e1, h)
 
     def _validate_input(self, G: nx.DiGraph, e1: str, h: str) -> None:
         """Validate the input to the path analysis."""
 
         if not G.has_node(e1):
             msg = f"{e1!r} is not a node in the graph"
-            logger.error(msg)
             raise ValueError(msg)
         if not G.has_node(h):
             msg = f"{h!r} is not a node in the graph"
-            logger.error(msg)
             raise ValueError(msg)
 
-
-    def _articulation_points(self, G: nx.Graph, s: str, h: str, d: int) -> None:
+    def _naive_solution(self, G: nx.DiGraph, e1: str, h: str) -> set[str]:
         """
-        Find articulation points in a graph.
+        A naive solution to the path analysis problem.
         :param G: The graph to search for articulation points.
-        :return: A set of articulation points.
+        :param e1: The start node.
+        :param h: The end node.
+        :return: A set of moust-go-through nodes.
         """
-        self.visited[s] = True
-        self.depth[s] = d
-        self.low[s] = d
-        if s == h: # Probably not needed since CFG ensures that h is reachable from e1
-            self.h_found = True
-            return
-        for k in G.neighbors(s):
-            if not self.visited[k]:
-                self._articulation_points(G, k, h, d + 1)
-            self.low[s] = min(self.low[s], self.low[k])
-            if self.low[k] >= self.depth[s] and k != h:
-                self.result.add(s)
+
+        def dfs(G: nx.DiGraph, start, path, all_paths):
+            path = path + [start]
+            if start == h:
+                all_paths.append(path)
+            else:
+                for node in G.neighbors(start):
+                    if (
+                        node not in path
+                    ):  # Can switch to defultdict(bool) to lower complexity
+                        dfs(G, node, path, all_paths)
+
+        all_paths: list[list] = []
+        dfs(G, e1, [], all_paths)
+
+        if not all_paths:
+            raise ValueError(f"No path from {e1!r} to {h!r}")
+
+        must_go_through = set(all_paths[0])
+        for path in all_paths[1:]:
+            must_go_through.intersection_update(path)
+
+        must_go_through.remove(h)
+        return must_go_through
--- src/api.py
+++ src/api.py
@@ -5,24 +5,27 @@ import networkx as nx
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+from algo import PathAnalyser
+
 app = FastAPI()
 
 # setup logging
 logger = logging.getLogger(__name__)
 
 
-class PathAnalysisRequest(BaseModel):
+class PathAnalysisRequestDTO(BaseModel):
     graph: str
     e1: str
     h: str
 
 
-def find_must_go_through_nodes(G: nx.DiGraph, e1: str, h: str) -> set[int]:
-    return {1, 2}  # Currently a stub for testing
+def find_must_go_through_nodes(G: nx.DiGraph, e1: str, h: str) -> set[str]:
+    algo = PathAnalyser()
+    return algo(G, e1, h)
 
 
 @app.post("/server")
-def process_graph(input: PathAnalysisRequest):
+def process_graph(input: PathAnalysisRequestDTO):
     try:
         G = parse_dot_to_digraph(input.graph)
         nodes = find_must_go_through_nodes(G, input.e1, input.h)
